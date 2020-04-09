a36565bef965b80ecdd689714feb03cc46afe4fa
ArtsiomPap <61845251+ArtsiomPap@users.noreply.github.com>
2020-04-09 23:09:31 +0300

    Add files via upload

--- task08/CustomerLogic.java
+++ task08/CustomerLogic.java
@@ -1,5 +1,6 @@
 package by.htp.classAndObj.task08;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,6 +8,26 @@ public class CustomerLogic {
 
 	public void customerListInAlphabeticalOrder(List<Customer> customers) {
 		Collections.sort(customers, new NameCustomersComparator());
-	
+
 	}
+
+	public List<Customer> customerNumberCardInterval(List<Customer> customers) {
+		List<Customer> customerNew = new ArrayList<Customer>();
+
+		for (Customer x : customers) {
+			if (x.getCreditCardNumber() > 666 & x.getCreditCardNumber() < 800) {
+				customerNew.add(x);
+
+			}
+		}
+	
+		return customerNew;
+	}
+	public void soutName(List<Customer> customers) {
+		for(Customer x :customers) {
+		System.out.println(x.getName());
+			
+		}
+	}
+
 }
--- task08/Main.java
+++ task08/Main.java
@@ -5,26 +5,30 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		Customer Vitya = new Customer(34313, "Zayats", "Vitya", "Anatolevich", "str:Ilimskaya,h0me:32,flat:54",
-				38822711, "abv245687");
+		Customer Vitya = new Customer(34313, "Zayats", "Vitya", "Anatolevich", "str:Ilimskaya,h0me:32,flat:54", 555,
+				"abv245687");
 
-		Customer Kolya = new Customer(34323, "Vredin", "Kolya", "Andreevich", "str:Ilimskaya,h0me:12,flat:53", 38822892,
+		Customer Kolya = new Customer(34323, "Vredin", "Kolya", "Andreevich", "str:Ilimskaya,h0me:12,flat:53", 680,
 				"abv245697");
 
-		Customer Andrey = new Customer(34333, "Pipkin", "Andrey", "Sergeevich", "str:Ilimskaya,h0me:14,flat:5",
-				38822891, "abv245657");
+		Customer Andrey = new Customer(34333, "Pipkin", "Andrey", "Sergeevich", "str:Ilimskaya,h0me:14,flat:5", 667,
+				"abv245657");
 
-		 Customers customers = new Customers();
+		Customer Zigmunt = new Customer(34313, "Zayats", "Zigmunt", "Anatolevich", "str:Ilimskaya,h0me:32,flat:54", 900,
+				"abv245617");
+
+		Customers customers = new Customers();
+		customers.add(Zigmunt);
 		customers.add(Andrey);
 		customers.add(Kolya);
 		customers.add(Vitya);
 
-		
-		
 		CustomerLogic customerLogic = new CustomerLogic();
-		customerLogic.customerListInAlphabeticalOrder( customers);
-	
-		System.out.println(customers);
+		customerLogic.customerListInAlphabeticalOrder( customers.getCustomers());// имена по алфавиту
+
+		customerLogic.soutName((customers.getCustomers()));
+		
+	//	customerLogic.soutName((customerLogic.customerNumberCardInterval(customers.getCustomers())));//имена чьи кредитки в интервале
 
 	}
 }
